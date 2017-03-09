9fb3920ff491f3bd0a1d6a000ef30afb15df1d5d
rahombus97 <cchomick@syr.edu>
2017-03-08 21:14:33 -0500

    first commit

--- battleship.py
+++ battleship.py
@@ -1,50 +1,5 @@
 ## a simple 5x5 grid 2-D battleship game I've created that account for different user inputs 
-
-#1.
-from random import randint
-
-board = []
-
-for x in range(0, 5):
-    board.append(["O"] * 5)
-
-def print_board(board):
-    for row in board:
-        print " ".join(row)
-
-print_board(board)
-
-#2.
-def random_row(board):
-    return randint(0, len(board) - 1)
-
-def random_col(board):
-    return randint(0, len(board) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-
-#3. 
-guess_row = int(raw_input("Guess Row:"))
-
-#4.
-guess_col = int(raw_input("Guess Col:"))
-
-#5.
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
-else:
-    if guess_row not in range(5) or \
-    guess_col not in range(5):
-        print "Oops, that's not even in the ocean."
-    elif board[guess_row][guess_col] == "X":
-        print "You guessed that one already."
-    else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
-        print_board(board)
-
-        
+      
 #1.        
 from random import randint
 
