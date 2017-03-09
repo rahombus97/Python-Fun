52b27de56b0ec358d2ca51ead0602fae26115a69
rahombus97 <cchomick@syr.edu>
2017-03-09 12:04:33 -0500

    first commit

--- battleship.py
+++ battleship.py
@@ -11,7 +11,7 @@ def print_board(board):
     for row in board:
         print " ".join(row)
 
-print "Let's play Battleship!"
+print "Battleship Time!"
 print_board(board)
 
 def random_row(board):
